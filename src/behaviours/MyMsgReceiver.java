185a26ca2d909fb2e5c8a0b5715abbc37d51b748
Steven Ly <Rauiy@bitbucket.org>
2017-11-14 15:15:40 +0100

    added behaviour package

---
+++ src/behaviours/MyMsgReceiver.java
@@ -0,0 +1,19 @@
+package behaviours;
+
+import jade.core.behaviours.Behaviour;
+
+/**
+ * Created by Steven on 2017-11-14.
+ */
+public class MyMsgReceiver extends Behaviour {
+
+    @Override
+    public void action() {
+
+    }
+
+    @Override
+    public boolean done() {
+        return false;
+    }
+}
---
+++ src/behaviours/MyTickerBehaviour.java
@@ -0,0 +1,14 @@
+package behaviours;
+import jade.core.Agent;
+import jade.core.behaviours.*;
+
+public class MyTickerBehaviour extends TickerBehaviour {
+    public MyTickerBehaviour(Agent a, long period) {
+        super(a, period);
+    }
+
+    @Override
+    protected void onTick() {
+
+    }
+}
