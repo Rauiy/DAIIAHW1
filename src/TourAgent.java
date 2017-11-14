7068bf29b39de3ab805bcc5240e6378bd862f613
Steven Ly <Rauiy@bitbucket.org>
2017-11-14 12:39:18 +0100

    curator and profiler are done for now

--- src/TourAgent.java
+++ src/TourAgent.java
@@ -5,7 +5,7 @@ import java.util.List;
  * Created by Steven on 2017-11-13.
  */
 public class TourAgent {
-    private HashMap<Integer,List<Artifacts>> tours;
+    private HashMap<Integer,List<Artifact>> tours;
 
     
 
